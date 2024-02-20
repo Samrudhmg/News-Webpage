a22f3de15775eff2dbda18b695454d8897740d04
Samrudh <samrudhmgowda61@gmail.com>
2024-02-20 12:17:20 +0530

    Updated

--- carnews/src/Components/Navbar.js
+++ carnews/src/Components/Navbar.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import "./navstyles.css"
 
 export default class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg" style={{backgroundColor:'#9EB384'}}>
+        <nav className="navbar navbar-expand-lg" >
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/" style={{fontSize:'1.5rem'}}>NeWsHUb</Link>
+            <Link className="navbar-brand" to="/" >NeWsHUb</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -19,7 +20,7 @@ export default class Navbar extends Component {
 
                 </li>
                 <li className="nav-item">
-                  <li>  <Link className="nav-link" to="/business">business</Link></li>
+                  <li>  <Link className="nav-link" to="/business">Business</Link></li>
                   
 
 
@@ -27,26 +28,23 @@ export default class Navbar extends Component {
                 </li>
                 <li><li> <Link className="nav-link" to="/sports">Sports</Link></li></li>
                 <li>
-                  <li> <Link className="nav-link" to="/entertainment">entertainment</Link></li>
+                  <li> <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
 
                 </li>
                 <li>
-                  <li>  <Link className="nav-link" to="/health">health</Link></li>
+                  <li>  <Link className="nav-link" to="/health">Health</Link></li>
 
                 </li>
                 <li>
-                  <li>  <Link className="nav-link" to="/science">science</Link></li>
+                  <li>  <Link className="nav-link" to="/science">Science</Link></li>
 
                 </li>
                 <li>
-                  <li>  <Link className="nav-link" to="/technology">technology</Link></li>
+                  <li>  <Link className="nav-link" to="/technology">Technology</Link></li>
                 </li>
               </ul>
 
-              <form className="d-flex" role="search">
-                <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                <button className="btn btn-outline-success" type="submit">Search</button>
-              </form>
+              
             </div>
           </div>
         </nav>
--- carnews/src/Components/Newsitem.js
+++ carnews/src/Components/Newsitem.js
@@ -1,4 +1,6 @@
+
 import React, { Component } from 'react'
+import "./navstyles.css"
 
 export default class Newsitem extends Component {
      
@@ -6,14 +8,14 @@ export default class Newsitem extends Component {
     render() {
         let {title, data,imageurl,newsurl,author,date }=this.props;
         return (
-            <div className='container1'>
+            <div className='container1' >
                 <div className='card' style={{width:'18rem'}}  >
-                <img src={imageurl?imageurl:'https://media.istockphoto.com/id/1254219019/vector/new-comic-speech-bubble-vector-flat-illustrations-color-phrase-lettering-with-explosive.jpg?s=612x612&w=0&k=20&c=5ohcYYNPS9IoYUSnQxcO0G6lN324dRzU5IKVjuLfxeI='} className="card-img-top" alt="..."/>
+                <img src={imageurl?imageurl:'noimage.png'} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{data}...</p>
+                        <p className="card-text">{data}....</p>
                         <p className="card-text"><small className="text-body-secondary">by {!author?"unknown":author} Last updated at {!date?new Date(date).toGTMString:date}</small></p>
-                        <a href={newsurl} className="btn btn-primary btn-sm">Browse news</a>
+                        <a href={newsurl} className="btn btn-primary btn-sm" style={{backgroundColor:'#3085C3'}}>Browse news</a>
                     </div>
                  </div>
             </div>
